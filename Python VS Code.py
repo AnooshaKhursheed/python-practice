2d19b1eb353ad668147798e21322a8c5a4ffa433
Anoosha Khursheed <misbhakhursheed721@gmail.com>
2026-06-07 17:57:21 +0500

    Python arithmetic practice

---
+++ Python VS Code.py
@@ -0,0 +1,8 @@
+a=20
+b=10
+print("the value of",20,"+",10,"is:", a+b)
+print("the value of",20,"/",10,"is:",a/b)
+print("the value of",20,"-",10,"is:",a-b)
+print("thae value of",20,"*",10,"is:",a*b)
+print("The value of",20,"//",10,"is:",a//b)
+print("the value of",20,"**",10,"is:",a**b)
