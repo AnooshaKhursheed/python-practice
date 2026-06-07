a490629fba4556c2410aac220174e46db5e83e6d
Anoosha Khursheed <misbhakhursheed721@gmail.com>
2026-06-07 19:51:34 +0500

    kya kiya likho

--- Python VS Code.py
+++ Python VS Code.py
@@ -1,8 +1,23 @@
-a=20
-b=10
-print("the value of",20,"+",10,"is:", a+b)
-print("the value of",20,"/",10,"is:",a/b)
-print("the value of",20,"-",10,"is:",a-b)
-print("thae value of",20,"*",10,"is:",a*b)
-print("The value of",20,"//",10,"is:",a//b)
-print("the value of",20,"**",10,"is:",a**b)
+a="1"
+b="3"
+print(int(a)+int(b))
+
+number=10
+string="200"
+sum=number+int(string)
+print(sum)
+
+list="apple,mango,orange"
+new_list="kiwi"
+sum=list+ " "+ "," +new_list
+print(sum)
+
+string="anoosha"
+message="hello"
+new= "hello"+ "  " +string
+print(new)
+
+birth_year= "2005"
+current_year=  2026
+calculate_age=(current_year - int(birth_year)) + 1
+print(calculate_age)
