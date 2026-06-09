576123e5abae573c0bf808bc278e2d76ead7e0e5
Anoosha Khursheed <misbhakhursheed721@gmail.com>
2026-06-09 18:58:59 +0500

    taking user input

---
+++ python practice 2.py
@@ -0,0 +1,16 @@
+a=input()
+print(a)
+
+a=input("enter your name:")
+print("my name is",a)
+
+b=input("what is your favourite kpop band?")
+print("my favourite kpop band is",b)
+
+x=input("enter first number:")
+y=input("enter your second number:")
+print(int(x) + int(y))
+
+a=input("enter your first name:")
+b=input("enter your second name:")
+print(input(a)+ input(b))
